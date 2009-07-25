ae640f1272fcab57e7b2c7a4dcb6216d3c885ae1
Brice Leroy <dev@kaaloo.com>
2009-07-25 17:35:09 +0000

    Make form for user linked to project dynamic

--- project/urls.py
+++ project/urls.py
@@ -8,6 +8,9 @@ urlpatterns = patterns('project.views',
     url(r'^spectator/(?P<project_id>\d+)/$', 'add_remove_spectator', name='add_remove_spectator'),
     url(r'^collaborator/(?P<project_id>\d+)/$', 'add_remove_collaborator', name='add_remove_collaborator'),
     url(r'^owner/(?P<project_id>\d+)/$', 'add_remove_owner', name='add_remove_owner'),
+    url(r'^owner_form/(?P<project_id>\d+)/$', 'owner_form', name='owner_form'),
+    url(r'^spectator_form/(?P<project_id>\d+)/$', 'spectator_form', name='spectator_form'),
+    url(r'^collaborator_form/(?P<project_id>\d+)/$', 'collaborator_form', name='collaborator_form'),
 )
 
 
